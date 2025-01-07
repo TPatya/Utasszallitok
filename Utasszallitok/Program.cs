7a4ed3e4601e0fd0fa324fdf4a44451d94e42188
szevasz <163414309+TPatya@users.noreply.github.com>
2025-01-07 13:16:27 +0100

    5.Feladat

--- Utasszallitok/Program.cs
+++ Utasszallitok/Program.cs
@@ -4,12 +4,27 @@
     {
         static void Main(string[] args)
         {
+            //4
+
             List<Repulo> Repulok = new();
             foreach (var sor in File.ReadAllLines("utasszallitok.txt").Skip(1))
             {
                 Repulok.Add(new Repulo(sor));
             }
             Console.WriteLine($"4.Feladat: \t Adatsorok száma: {Repulok.Count()}");
+
+            //5
+
+            int db = 0;
+            foreach (var b in Repulok)
+            {
+                if (b.Tipus.StartsWith("Boeing"))
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine($"5.Feladat \t Boeing Típusok száma: {db}");
+
         }
     }
 }
